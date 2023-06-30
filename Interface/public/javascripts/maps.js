73af66ca82dfd1864707cb55682c46a3155a1469
Ricardo Araújo <ricardoaraujo@Ricardos-Air.home>
2023-06-30 21:17:42 +0100

    Maps nas ruas

--- Interface/public/javascripts/maps.js
+++ Interface/public/javascripts/maps.js
@@ -1,10 +1,22 @@
 let map;
 
+var mapPositionLat = 0
+var mapPositionLng = 0
+
+
+function setCoords(lat,lng){
+    console.log('Coordinates:', lat, lng);
+    mapPositionLat = parseFloat(lat.substring(1,lat.length-1));
+    mapPositionLng = parseFloat(lng.substring(1,lat.length-1));
+    console.log('Coordinates:', mapPositionLat, mapPositionLng);
+    
+}
+
 async function initMap() {
   const { Map } = await google.maps.importLibrary("maps");
 
   map = new Map(document.getElementById("map"), {
-    center: { lat: 41.544494531853466, lng: -8.426232643797164 },
+    center: { lat: mapPositionLat, lng: mapPositionLng },
     zoom: 15,
   });
 }
